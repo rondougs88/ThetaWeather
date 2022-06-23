0c689f8b96519c9a456818399d0189b591771a0c
Ron@nativesoftware.co.nz <Ron@nativesoftware.co.nz>
2022-06-23 18:55:41 +0800

    extract data within pre tag

--- ThetaWeather/Program.cs
+++ ThetaWeather/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ThetaWeather
 {
@@ -10,7 +11,40 @@ namespace ThetaWeather
     {
         static void Main(string[] args)
         {
+            StreamReader oReader = new StreamReader("..\\..\\Files\\weather.txt", Encoding.UTF8);
+            string inputLine = "";
+            string sPreContent = "";
 
+            bool bAppendLine = false;
+            //RL: Loop through each line of the txt file
+            while ((inputLine = oReader.ReadLine()) != null)
+            {
+                if (bAppendLine && !inputLine.Trim().StartsWith("</"))
+                {
+                    sPreContent += inputLine + "\n";
+                }
+
+                if (inputLine.Trim().StartsWith("<"))
+                {
+                    
+                    if (bAppendLine)    //RL: Condition is true when we get to the closing tag
+                    {
+                        bAppendLine = false;
+                    }
+                    else //RL: Condition is true when we get to the opening tag
+                    {
+                        bAppendLine = true;
+                        //sPreContent += inputLine + "\n";
+                    }
+                    
+                }
+
+            }
+
+
+
+            Console.Write(sPreContent);
+            Console.ReadLine();
         }
     }
 }
